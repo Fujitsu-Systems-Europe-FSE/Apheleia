ebba58b06737380190cab00b207a799d82076f42
Julien Wallart <julien.wallart@outlook.com>
2023-06-29 11:51:58 +0200

    Added visualization tools / gradients manipulation functions

---
+++ apheleia/utils/gradients.py
@@ -0,0 +1,13 @@
+from torch import autograd
+
+import torch
+
+def calc_jacobian_norm(output, inputs):
+    gradients = autograd.grad(output.sum(), inputs, create_graph=True, retain_graph=True)
+    gradients = torch.cat([g.flatten(start_dim=1) for g in gradients], dim=0).detach()
+    return gradients.norm(2, dim=1).cpu().numpy()
+
+def calc_net_gradient_norm(network):
+    gradients = [v.grad for k, v in network.named_parameters() if v.grad is not None]
+    norms = torch.stack([g.flatten().norm(2) for g in gradients]).detach()
+    return norms.cpu().numpy()
---
+++ apheleia/utils/visualize.py
@@ -0,0 +1,26 @@
+from typing import List
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
+
+def gradients_norm_hist(writer, title, grads_norm: List[np.ndarray], epoch, n_bins=100, labels=None):
+    if labels is not None:
+        assert len(labels) == len(grads_norm), 'Number of labels mismatch number of gradients norm'
+
+    fig, ax = plt.subplots(1, 1, sharey=True, tight_layout=True)
+    fig.suptitle('Gradients norm histogram')
+
+    color_table = list(mcolors.TABLEAU_COLORS.keys())
+
+    for i in range(len(grads_norm)):
+        label = labels if labels is None else labels[i]
+        ax.hist(grads_norm[i], bins=n_bins, color=color_table[i % len(grads_norm)], alpha=.5, label=label)
+
+    ax.set_xlabel('norm value')
+    ax.set_ylabel('count')
+    ax.legend()
+
+    writer.add_figure(f'gradients/{title}', fig, global_step=epoch)
+    writer.flush()
