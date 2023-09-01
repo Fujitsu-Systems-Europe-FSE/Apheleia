c49d42ec4dfacb5c6698b9a1ca35bbe689dc72ed
Julien Wallart <julien.wallart@outlook.com>
2023-09-01 09:38:27 +0200

    Reworked dataset creation

--- apheleia/dataset/__init__.py
+++ apheleia/dataset/__init__.py
@@ -1,12 +1,13 @@
 from torch.utils import data
-from torch.utils.data import DistributedSampler
-from torchvision import transforms
 from apheleia import ProjectLogger
+from torchvision import transforms
+from torch.utils.data import DistributedSampler
+from .abstract_dataset import ImageDataset
 
 
-def get_dataset(dataset_class, dataset_type, path, args):
+def build_dataset(dataset_class, path, args):
     trans = []
-    if dataset_type == 'image':
+    if type(dataset_class) == ImageDataset:
         args.means = [0.485, 0.456, 0.406] if 'means' not in args or args.means is None else args.means
         args.stds = [0.229, 0.224, 0.225] if 'stds' not in args or args.stds is None else args.stds
 
@@ -14,22 +15,19 @@ def get_dataset(dataset_class, dataset_type, path, args):
             args.im_size = args.im_size if isinstance(args.im_size, tuple) else (args.im_size, args.im_size)
             w, h = args.im_size
             trans = [transforms.Resize((h, w))]
-    elif dataset_type == 'serie':
-        raise NotImplemented()
-    # else:
 
-    # trans += [
-    #     transforms.ToTensor(),  # range [0;1]
-    #     transforms.Normalize(args.means, args.stds)
-    # ]
+        trans += [
+            transforms.ToTensor(),  # range [0;1]
+            transforms.Normalize(args.means, args.stds)
+        ]
 
     # dat = dataset_class(path, **{'transform': transforms.Compose(trans)})
-    # return dat, dataset_type
+
     return dataset_class(args, path)
 
 
-def get_dataloader(dataset_class, dataset_type, path, batch_size, workers, args, dataset_fn=get_dataset, collate_fn=None):
-    dataset = dataset_fn(dataset_class, dataset_type, path, args)
+def build_dataloader(dataset_class, path, batch_size, workers, args, dataset_factory_fn=build_dataset, collate_fn=None):
+    dataset = dataset_factory_fn(dataset_class, path, args)
     opts = {
         'batch_size': batch_size,
         'drop_last': False,
--- apheleia/dataset/abstract_dataset.py
+++ apheleia/dataset/abstract_dataset.py
@@ -3,6 +3,23 @@ from abc import ABCMeta, abstractmethod
 
 
 class AbstractDataset(Dataset, metaclass=ABCMeta):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+
     @abstractmethod
     def num_features(self):
         pass
+
+
+class ImageDataset(AbstractDataset):
+    def num_features(self):
+        pass
+
+
+class VectorDataset(AbstractDataset):
+    pass
+
+
+class Memory(AbstractDataset):
+    pass
