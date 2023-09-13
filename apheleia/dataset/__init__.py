84ed731c8bd86787a843f2262e43a873f0bd6009
Julien Wallart <julien.wallart@outlook.com>
2023-09-13 15:30:36 +0200

    Dataset sampler can be customized

--- apheleia/dataset/__init__.py
+++ apheleia/dataset/__init__.py
@@ -5,6 +5,11 @@ from torch.utils.data import DistributedSampler
 from .abstract_dataset import ImageDataset
 
 
+def build_sampler(args, dataset):
+    sampler = DistributedSampler(dataset) if args.distributed else None
+    return sampler
+
+
 def build_dataset(dataset_class, path, args):
     trans = []
     if type(dataset_class) == ImageDataset:
@@ -26,14 +31,15 @@ def build_dataset(dataset_class, path, args):
     return dataset_class(args, path)
 
 
-def build_dataloader(dataset_class, path, batch_size, workers, args, dataset_factory_fn=build_dataset, collate_fn=None):
+def build_dataloader(dataset_class, path, batch_size, workers, args, dataset_factory_fn=build_dataset, sampler_fn=build_sampler, collate_fn=None):
     dataset = dataset_factory_fn(dataset_class, path, args)
+    sampler = sampler_fn(args, dataset)
     opts = {
         'batch_size': batch_size,
         'drop_last': False,
-        'shuffle': True if not args.distributed else False,
+        'shuffle': True if sampler is None else False,
         'num_workers': workers,
-        'sampler': DistributedSampler(dataset) if args.distributed else None
+        'sampler': sampler
     }
     if collate_fn is not None:
         opts['collate_fn'] = collate_fn
