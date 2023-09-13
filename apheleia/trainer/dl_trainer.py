54bf4e1a2e39cee8d797db44785f00b84a8511ca
Julien Wallart <julien.wallart@outlook.com>
2023-09-13 15:30:09 +0200

    Fixed broken imports

--- apheleia/trainer/dl_trainer.py
+++ apheleia/trainer/dl_trainer.py
@@ -3,7 +3,6 @@ from abc import ABC, abstractmethod
 from apheleia.trainer.trainer import Trainer
 from apheleia.utils.logger import ProjectLogger
 
-import torch
 import torch.distributed as dist
 
 
@@ -14,10 +13,10 @@ class DLTrainer(Trainer, ABC):
         pass
 
     def _b_tick(self):
-        self._batch_tick = time.time()
+        self._batch_tick = time()
 
     def _b_duration(self):
-        return time.time() - self._batch_tick
+        return time() - self._batch_tick
 
     def _log_iteration(self, batch_idx):
         if self.global_iter % self._log_interval == 0:
@@ -26,14 +25,13 @@ class DLTrainer(Trainer, ABC):
             iter_stats = 'exec time: {:.2f} second(s) speed: {:.2f} samples/s'.format(b_time, speed)
             ProjectLogger().info('[Epoch {}] --[{}/{}]-- {}'.format(self.current_epoch, batch_idx, self.num_iter, iter_stats))
 
-    @abstractmethod
-    def _report_graph(self):
-        # if self.current_epoch == 1 and self.writer is not ...:
-        #     graph = self.get_graph()
-        #     x: torch.Tensor = torch.zeros((1, self._opts.num_features, *self._opts.im_size)).to(self._ctx[0])
-        #     self.writer.add_graph(graph, x)
-        #     self.writer.flush()
-        pass
+    # @abstractmethod
+    # def _report_graph(self):
+    #     if self.current_epoch == 1 and self.writer is not ...:
+    #         graph = self.get_graph()
+    #         x: torch.Tensor = torch.zeros((1, self._opts.num_features, *self._opts.im_size)).to(self._ctx[0])
+    #         self.writer.add_graph(graph, x)
+    #         self.writer.flush()
 
     def _train_loop(self, train_data, *args, **kwargs):
         for batch_idx, batch in enumerate(train_data, start=1):
